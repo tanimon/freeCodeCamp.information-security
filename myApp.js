e19ddacb9959e8a780dfdf1631f0906b2555ee1c
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-23 20:28:35 +0900

    Add middleware to set a Content Security Policy

--- myApp.js
+++ myApp.js
@@ -16,6 +16,14 @@ app.use(
 );
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    },
+  })
+);
 
 module.exports = app;
 const api = require('./server.js');
