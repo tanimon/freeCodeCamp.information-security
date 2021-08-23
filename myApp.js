3ac540c4649be7a49f5e7963e88d92551dface40
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-23 20:39:23 +0900

    Refactor middlewares usig the parent `helmet()` middleware

--- myApp.js
+++ myApp.js
@@ -2,25 +2,22 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(
-  helmet.hsts({
-    maxAge: ninetyDaysInSeconds,
-    force: true,
-  })
-);
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com'],
+  helmet({
+    frameguard: {
+      action: 'deny',
+    },
+    hsts: {
+      maxAge: ninetyDaysInSeconds,
+      force: true,
+    },
+    noCache: true,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-cdn.com'],
+      },
     },
   })
 );
