d1df2127ccc521eaacfb33fec9351e60a6c446c1
tanimon <8575113+tanimon@users.noreply.github.com>
2021-09-10 19:04:08 +0900

    Tweak `server.py`
    - Add logging message of received connection
    - Specify utf-8 as encoding of message

--- 02_python-for-penetration-testing/server.py
+++ 02_python-for-penetration-testing/server.py
@@ -12,8 +12,9 @@ server_socket.listen(3)
 
 while True:
     client_socket, addr = server_socket.accept()
+    print(f"received connection from {addr}")
     
     message = "Hello! Thank you connecting the server!\n"
-    client_socket.send(message.encode())
+    client_socket.send(message.encode('utf-8'))
 
     client_socket.close()
