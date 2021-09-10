bbf367f6386c2178f5558e1b98387c534f1a55d2
tanimon <8575113+tanimon@users.noreply.github.com>
2021-09-11 07:27:31 +0900

    Add simple nmap scanner util

---
+++ 02_python-for-penetration-testing/nmap-scanner/scanner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool")
+print("<----------------------------------------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan \n""")
+print("You have selected option: ", resp)
+
+if resp == "1":
+    print(f"Nmap Version: {scanner.nmap_version()}")
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == "2":
+    print(f"Nmap Version: {scanner.nmap_version()}")
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == "3":
+    print(f"Nmap Version: {scanner.nmap_version()}")
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+    print("\n\nService Info: ", scanner[ip_addr]['tcp'])
+else:
+    print("You have entered an invalid option")
